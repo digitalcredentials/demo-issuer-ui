35b618adee2f3641c642ad87d9903ab476272c3d
James Chartrand <jc.chartrand@gmail.com>
2025-02-20 09:49:09 -0500

    fix request params

--- app/collect/page.tsx
+++ app/collect/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import AcmeLogo from '@/app/ui/dcc-logo';
+import DCCLogo from '@/app/ui/dcc-logo';
 import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation'
 import QRCode from "react-qr-code";
@@ -9,8 +9,8 @@ export default function Page() {
 
   const searchParams = useSearchParams()
  
-  const exchangeId = searchParams.get('exchange')
-  const transactionId = searchParams.get('transaction')
+  const exchangeId = searchParams.get('exchangeId')
+  const transactionId = searchParams.get('transactionId')
 
   const deepLink = `https://lcw.app/request.html?issuer=issuer.example.com&auth_type=bearer&challenge=50991c0d-e033-49c4-86aa-7f3620cf6937&vc_request_url=https://issuer.dcconsortium.org/exchange/${exchangeId}/${transactionId}`
   
@@ -19,11 +19,12 @@ export default function Page() {
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
+            <DCCLogo />
           </div>
         </div>
         <Suspense>
             Collect your credential!!!!!! Deep link and QR and mayber chapi will go here.
+            <br/><br/>
             <a href={`${deepLink}`}>Add your credential to the Learner Credential Wallet</a>
             <QRCode value={deepLink} />
         </Suspense>
@@ -31,5 +32,3 @@ export default function Page() {
     </main>
   );
 }
-
-// <QRCode value={deepLink} />
